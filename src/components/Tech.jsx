d04653f79b25623477c8e66178e1e0721717386e
Abduraimbek <yarkinovabduraimbek@gmail.com>
2023-07-30 15:30:20 +0300

    initial

---
+++ src/components/Tech.jsx
@@ -0,0 +1,19 @@
+import { BallCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { technologies } from '../constants';
+
+const Tech = () => {
+  return (
+    <SectionWrapper idName=''>
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name + 'technologies'}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </SectionWrapper>
+  );
+};
+
+export default Tech;
